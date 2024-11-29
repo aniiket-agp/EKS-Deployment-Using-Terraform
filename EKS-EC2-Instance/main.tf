179eda8938ec93484befe45143ce0286c017b06e
aniiket-agp <aniket.sapsphinx@gmail.com>
2024-11-29 10:22:57 +0530

    Update main.tf

--- EKS-EC2-Instance/main.tf
+++ EKS-EC2-Instance/main.tf
@@ -31,7 +31,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "eks-using-jenkins-cluster"
+  cluster_name    = "eks-cluster"
   cluster_version = "1.30"
   cluster_endpoint_public_access = true
   vpc_id     = module.vpc.vpc_id
